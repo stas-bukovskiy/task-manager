524b707a7929f02dad6d72595fa9da452860fcdd
stas-bk <s.bukovskyi@aumify.io>
2024-01-19 23:01:42 +0200

    feat: add configurable cors

--- updates/src/main/java/org/tasker/updates/config/WebConfig.java
+++ updates/src/main/java/org/tasker/updates/config/WebConfig.java
@@ -1,5 +1,7 @@
 package org.tasker.updates.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
@@ -9,13 +11,19 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import org.tasker.updates.input.http.AuthHandler;
 
+import java.util.Arrays;
+
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
+@Slf4j
 @Configuration
 @EnableWebFlux
 public class WebConfig implements WebFluxConfigurer {
+
+    @Value("${allowed.origins}")
+    private String[] allowedOrigins;
 
     @Bean
     RouterFunction<ServerResponse> routerFunction(AuthHandler handler) {
@@ -27,8 +35,13 @@ public class WebConfig implements WebFluxConfigurer {
     }
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("*");
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        log.info("Allowed origins: {}", Arrays.toString(allowedOrigins));
+
+        corsRegistry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("POST", "OPTIONS", "HEAD")
+                .maxAge(3600);
     }
 
 }
