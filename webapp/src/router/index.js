3277a763292f48e742f8f02c7db72bac935197b8
stas-bk <s.bukovskyi@aumify.io>
2024-04-03 19:39:04 +0300

    feat: implement auth logic

--- webapp/src/router/index.js
+++ webapp/src/router/index.js
@@ -1,11 +1,25 @@
 import {route} from 'quasar/wrappers'
 import {createRouter, createWebHistory} from 'vue-router'
 import routes from './routes'
-
+import {useStore} from "vuex";
 
 export default route(function (/* { store, ssrContext } */) {
-  return createRouter({
+  const router = createRouter({
     history: createWebHistory('/'),
     routes,
   })
+
+  router.beforeEach((to, from, next) => {
+    const store = useStore()
+    const isNotAuthenticated = !store.state.auth.token;
+
+    if (isNotAuthenticated && !to.path.startsWith("/auth")) {
+      next('/auth/login');
+    } else {
+      next();
+    }
+  });
+
+
+  return router
 })
